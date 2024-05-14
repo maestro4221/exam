de7c72cc3b5299d91f49f33f1f1a7704424446c1
maestro4221 <91077069+maestro4221@users.noreply.github.com>
2024-05-15 02:31:53 +0300

    made

--- source/app/urls.py
+++ source/app/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 
-from main.views import IndexPageView, ChangeLanguageView
+from main.views import IndexPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,7 +11,6 @@ urlpatterns = [
     path('', IndexPageView.as_view(), name='index'),
 
     path('i18n/', include('django.conf.urls.i18n')),
-    path('language/', ChangeLanguageView.as_view(), name='change_language'),
 
     path('accounts/', include('accounts.urls')),
 ]
--- source/main/views.py
+++ source/main/views.py
@@ -4,6 +4,3 @@ from django.views.generic import TemplateView
 class IndexPageView(TemplateView):
     template_name = 'main/index.html'
 
-
-class ChangeLanguageView(TemplateView):
-    template_name = 'main/change_language.html'
